e41170d182fb9b4a43af21b1d1cf36b297d81449
Renata Guedes <renataleitesg@gmail.com>
2024-08-26 16:30:40 -0300

    Update attendance form component

--- src/app/components/attendance-form/attendance-form.component.ts
+++ src/app/components/attendance-form/attendance-form.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Attendance } from 'src/app/models/attendance.models';
+import { CollaboratorService } from 'src/app/services/collaborator.service';
 import { AttendanceService } from 'src/app/services/attendance.service';
+import { Collaborator } from 'src/app/models/collaborator.model';
 
 @Component({
   selector: 'app-attendance-form',
@@ -11,36 +12,94 @@ import { AttendanceService } from 'src/app/services/attendance.service';
 })
 export class AttendanceFormComponent implements OnInit {
   attendanceForm: FormGroup;
+  collaborators: Collaborator[] = [];
+  minDate: string;
+  maxDate: string;
 
   constructor(
     private fb: FormBuilder,
+    private collaboratorService: CollaboratorService,
     private attendanceService: AttendanceService,
     private router: Router
   ) {
+    const today = new Date();
+    this.minDate = this.formatDate(today);
+    this.maxDate = this.formatDate(this.addDays(today, 30)); // Limite de 30 dias a partir de hoje
     this.attendanceForm = this.fb.group({
       collaboratorId: ['', Validators.required],
-      checkInTime: ['', Validators.required],
-      checkOutTime: ['', Validators.required]
+      attendanceDate: [this.minDate, Validators.required],
+      checkInTime: ['08:00', Validators.required],
+      checkOutTime: ['18:00', Validators.required]
     });
   }
 
   ngOnInit(): void {
+    this.loadCollaborators();
   }
 
-  onSubmit(): void {
-    if (this.attendanceForm.valid) {
-      const formValues = this.attendanceForm.value;
-      
-      const attendance: Attendance = {
-        collaboratorId: formValues.collaboratorId,
-        checkInTime: new Date(formValues.checkInTime),
-        checkOutTime: new Date(formValues.checkOutTime),
-      };
+  loadCollaborators(): void {
+    this.collaboratorService.getCollaborators().subscribe(collaborators => {
+      this.collaborators = collaborators;
+    });
+  }
 
-      this.attendanceService.createAttendance(attendance).subscribe(() => {
-        alert('Registro de ponto criado com sucesso!');
-        this.router.navigate(['/attendances']);
-      });
+  onSubmit(): void {  
+    const formValues = this.attendanceForm.value;
+    // Construindo a string de data manualmente para manter o horário local
+    const checkInTime = `${formValues.attendanceDate}T${formValues.checkInTime}`;
+    const checkOutTime = `${formValues.attendanceDate}T${formValues.checkOutTime}`;
+  
+    this.attendanceService.createAttendance({
+      collaboratorId: formValues.collaboratorId,
+      checkInTime: checkInTime,
+      checkOutTime: checkOutTime
+    }).subscribe(() => {
+      alert('Registro de ponto criado com sucesso!');
+      this.router.navigate(['/lista-de-pontos']);
+    },
+    (error) => {
+      console.error('Erro ao criar o registro de ponto:', error);
+      alert('Falha ao criar o registro de ponto. Por favor, tente novamente.');
+    });
+  }
+  
+  
+
+  formatDate(date: Date): string {
+    return date.toISOString().split('T')[0];
+  }
+
+  addDays(date: Date, days: number): Date {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  onDateChange(event: any): void {
+    // Obtendo o valor da data selecionada no formato de string (ano-mês-dia)
+    const selectedDateString = event.target.value;
+  
+    // Criando um novo objeto Date com a data selecionada como uma data local
+    const selectedDate = new Date(selectedDateString + 'T00:00:00');
+  
+    console.log(selectedDate);
+  
+    // Verificando se a data selecionada é sábado ou domingo
+    if (selectedDate.getDay() === 0 || selectedDate.getDay() === 6) {
+      // Se for domingo (0) ou sábado (6), define um erro
+      this.attendanceForm.get('attendanceDate')?.setErrors({ 'invalidDate': true });
+    } else {
+      // Se for de segunda (1) a sexta (5), remove o erro
+      this.attendanceForm.get('attendanceDate')?.setErrors(null);
     }
+  }
+  
+  onBack(): void {
+    this.router.navigate(['/lista-de-pontos']); // Navega para a página de lista de registros de pontos
+  }
+  
+  isValidTime(time: string): boolean {
+    // Função que determina se o horário está dentro do intervalo permitido
+    return time >= '08:00' && time <= '18:00';
   }
 }
