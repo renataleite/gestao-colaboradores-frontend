1a0c86b6a581ad65c21f6076f7cd8cd64b4bfa63
Renata Guedes <renataleitesg@gmail.com>
2024-08-30 08:08:43 -0300

    chore: Update collaborator edit component to display success and error messages with snackbar

--- src/app/components/collaborator-edit/collaborator-edit.component.ts
+++ src/app/components/collaborator-edit/collaborator-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CollaboratorService } from '../../services/collaborator.service';
 import { Collaborator } from '../../models/collaborator.model';
 
@@ -17,7 +18,8 @@ export class CollaboratorEditComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private fb: FormBuilder,
-    private collaboratorService: CollaboratorService
+    private collaboratorService: CollaboratorService,
+    private snackBar: MatSnackBar
   ) {
     this.collaboratorForm = this.fb.group({
       name: ['', Validators.required],
@@ -47,9 +49,21 @@ export class CollaboratorEditComponent implements OnInit {
       this.collaboratorService.updateCollaborator(this.collaboratorId, updatedCollaborator).subscribe(
         () => {
           console.log('Colaborador atualizado com sucesso');
+          this.snackBar.open('Colaborador atualizado com sucesso!', '', {
+            duration: 3000,
+            verticalPosition: 'top',
+            horizontalPosition: 'right'
+          });
           this.router.navigate(['/colaboradores']);
         },
-        error => console.error('Erro ao atualizar colaborador', error)
+        error => {
+          console.error('Erro ao atualizar colaborador', error);
+          this.snackBar.open('Erro ao atualizar colaborador. Tente novamente.', '', {
+            duration: 3000,
+            verticalPosition: 'top',
+            horizontalPosition: 'right'
+          });
+        }
       );
     }
   }
